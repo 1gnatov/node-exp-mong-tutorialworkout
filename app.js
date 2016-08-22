09fb07b7d223bde36e2211a803aa64f3b37f86a2
1gnatov <ignatov.msu@gmail.com>
2016-08-22 13:21:52 +0300

    user router refactor and added middleware for authcheck

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 
 var routes = require('./routes/index');
+var userRoutes = require('./routes/users');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/user', userRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
